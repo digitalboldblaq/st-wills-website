2db15dd21a2f2bf7b760dd8805d4c7ece1e201f6
Boldblaq <boldblaq@gmail.com>
2026-04-27 21:21:48 +0100

    Added facilities section and optimized images

--- js/script.js
+++ js/script.js
@@ -28,6 +28,20 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+// FACILITIES LIGHTBOX
+
+function openLightbox(img) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  lightbox.style.display = "flex";
+  lightboxImg.src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
+
 //TESTIMONAL SLIDER
 document.addEventListener("DOMContentLoaded", () => {
   const track = document.querySelector(".testimonial-track");
